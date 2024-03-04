f59bb4fca25b36b4a3356dfcffa721a4eaad1f56
leeddwcs <ducle@chidoanh.com>
2024-03-04 23:46:12 +0700

    feat: connect to github

--- src/components/my-stamps.tsx
+++ src/components/my-stamps.tsx
@@ -70,37 +70,29 @@ export function Stamp() {
         <DrawerContent>
           <div className="mx-auto w-full max-w-sm">
             <DrawerHeader>
-              <DrawerTitle>Move Goal</DrawerTitle>
-              <DrawerDescription>Set your daily activity goal.</DrawerDescription>
+              <DrawerTitle>Github</DrawerTitle>
+              <DrawerDescription>6.9</DrawerDescription>
             </DrawerHeader>
-            <div className="p-4 pb-0">
-              <div className="flex items-center justify-center space-x-2">
-                <Button
-                  variant="outline"
-                  size="icon"
-                  className="h-8 w-8 shrink-0 rounded-full"
-                  onClick={() => console.log('CLICK')}
-                >
-                  <Minus className="h-4 w-4" />
-                  <span className="sr-only">Decrease</span>
-                </Button>
-                <div className="flex-1 text-center">
-                  <div className="text-7xl font-bold tracking-tighter">{6.92}</div>
-                  <div className="text-[0.70rem] uppercase text-muted-foreground">Calories/day</div>
-                </div>
-                <Button
-                  variant="outline"
-                  size="icon"
-                  className="h-8 w-8 shrink-0 rounded-full"
-                  onClick={() => console.log('CLICK')}
-                >
-                  <Plus className="h-4 w-4" />
-                  <span className="sr-only">Increase</span>
-                </Button>
-              </div>
-            </div>
             <DrawerFooter>
-              <Button>Submit</Button>
+              <Button
+                onClick={() => {
+                  const rootURl = 'https://github.com/login/oauth/authorize';
+
+                  const options = {
+                    client_id: '5f5991f94e3f8e1224df',
+                    redirect_uri: 'http://localhost:5173/dashboard',
+                    scope: 'user:email',
+                    state: location.pathname
+                  };
+
+                  const qs = new URLSearchParams(options);
+                  const url = `${rootURl}?${qs.toString()}`;
+                  console.log(url);
+                  window.location.href = url;
+                }}
+              >
+                Connect
+              </Button>
               <DrawerClose asChild>
                 <Button variant="outline">Cancel</Button>
               </DrawerClose>

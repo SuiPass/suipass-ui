40b542f08c193553eec4023f491c7f66ed261c9b
NguyenTrongTin <ntrongtin11702@gmail.com>
2024-03-21 11:46:10 +0700

    fix: github flow

--- src/repositories/request-repository.ts
+++ src/repositories/request-repository.ts
@@ -5,10 +5,13 @@ import axios from 'axios';
 class RequestRepository extends Repository {
   async list(payload?: { provider: string }) {
     console.info(this.constructor.name, { list: payload });
-    const res = await axios.get(`${SUIPASS_API_URL}/request/list`, {
+    const res = await axios.get(`${SUIPASS_API_URL}/requests`, {
       params: {
         address: this.account.address,
         provider: payload?.provider,
+      },
+      headers: {
+        'x-wallet-address': this.account.address,
       },
     });
 
@@ -17,9 +20,12 @@ class RequestRepository extends Repository {
 
   async create(payload?: { provider: string }) {
     console.info(this.constructor.name, { create: payload });
-    await axios.post(`${SUIPASS_API_URL}/request/create`, payload, {
+    await axios.post(`${SUIPASS_API_URL}/requests`, payload, {
       params: {
         address: this.account.address,
+      },
+      headers: {
+        'x-wallet-address': this.account.address,
       },
     });
   }
